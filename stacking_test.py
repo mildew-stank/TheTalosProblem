9bbab6dd2b9b923a166e71a15298a09f90d34558
mildew-stank <139667119+mildew-stank@users.noreply.github.com>
2025-04-05 07:29:41 -0400

    Update stacking_test.py

--- stacking_test.py
+++ stacking_test.py
@@ -11,7 +11,9 @@ class TestStacking(unittest.TestCase):
         self.assertEqual(world.get_max_height(8), 4)
         self.assertEqual(world.get_max_height(11), 5)
         self.assertEqual(world.get_max_height(15), 6)
-        self.assertEqual(world.get_max_height(32), 9)
+        self.assertEqual(world.get_max_height(20), 7)
+        self.assertEqual(world.get_max_height(25), 8)
+        self.assertEqual(world.get_max_height(31), 9)
         self.assertEqual(world.get_max_height(0), -1)
         self.assertEqual(world.get_max_height(-1), -1)
 
@@ -23,7 +25,9 @@ class TestStacking(unittest.TestCase):
         self.assertEqual(world.get_min_boxes(4), 8)
         self.assertEqual(world.get_min_boxes(5), 11)
         self.assertEqual(world.get_min_boxes(6), 15)
-        self.assertEqual(world.get_min_boxes(9), 32)
+        self.assertEqual(world.get_min_boxes(7), 20)
+        self.assertEqual(world.get_min_boxes(8), 25)
+        self.assertEqual(world.get_min_boxes(9), 31)
         self.assertEqual(world.get_min_boxes(0), -1)
         self.assertEqual(world.get_min_boxes(-1), -1)
 
