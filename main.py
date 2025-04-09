5b87d995d9388797a88df499abc386b2cb5cc0a8
mildew-stank <139667119+mildew-stank@users.noreply.github.com>
2025-04-09 02:00:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from stacking import TheTalosProblem as ttp
+from stacking import Solution
 
 
 def main() -> None:
-    world = ttp()
+    stack = Solution()
     while True:
         user_input = input("Enter required stack height: ")
         if user_input.lower() in ["exit", "quit"]:
@@ -15,9 +15,9 @@ def main() -> None:
         if n <= 0:
             print("Integer must be greater than 0.")
             continue
-        print("Minimum boxes required:", world.get_min_boxes(n))
+        print("Minimum boxes required:", stack.get_min_blocks(n))
         print("Final stack:")
-        out = [i for i in world.world_space if i != 0]
+        out = [i for i in stack.world if i != 0]
         print(out)
 
 
