e2801c26b0b136752878f6631d337e16cea1e84f
mildew-stank <139667119+mildew-stank@users.noreply.github.com>
2025-04-09 02:01:16 -0400

    Update stacking_test.py

--- stacking_test.py
+++ stacking_test.py
@@ -1,35 +1,35 @@
 import unittest
-from stacking import TheTalosProblem as ttp
+from stacking import Solution
 
 
 class TestStacking(unittest.TestCase):
     def test_get_max_height(self):
-        world = ttp()
-        self.assertEqual(world.get_max_height(1), 1)
-        self.assertEqual(world.get_max_height(3), 2)
-        self.assertEqual(world.get_max_height(5), 3)
-        self.assertEqual(world.get_max_height(8), 4)
-        self.assertEqual(world.get_max_height(11), 5)
-        self.assertEqual(world.get_max_height(15), 6)
-        self.assertEqual(world.get_max_height(20), 7)
-        self.assertEqual(world.get_max_height(25), 8)
-        self.assertEqual(world.get_max_height(31), 9)
-        self.assertEqual(world.get_max_height(0), -1)
-        self.assertEqual(world.get_max_height(-1), -1)
+        stack = Solution()
+        self.assertEqual(stack.get_max_height(1), 1)
+        self.assertEqual(stack.get_max_height(3), 2)
+        self.assertEqual(stack.get_max_height(5), 3)
+        self.assertEqual(stack.get_max_height(8), 4)
+        self.assertEqual(stack.get_max_height(11), 5)
+        self.assertEqual(stack.get_max_height(15), 6)
+        self.assertEqual(stack.get_max_height(20), 7)
+        self.assertEqual(stack.get_max_height(25), 8)
+        self.assertEqual(stack.get_max_height(31), 9)
+        self.assertEqual(stack.get_max_height(0), 0)
+        self.assertEqual(stack.get_max_height(-1), 0)
 
     def test_get_min_boxes(self):
-        world = ttp()
-        self.assertEqual(world.get_min_boxes(1), 1)
-        self.assertEqual(world.get_min_boxes(2), 3)
-        self.assertEqual(world.get_min_boxes(3), 5)
-        self.assertEqual(world.get_min_boxes(4), 8)
-        self.assertEqual(world.get_min_boxes(5), 11)
-        self.assertEqual(world.get_min_boxes(6), 15)
-        self.assertEqual(world.get_min_boxes(7), 20)
-        self.assertEqual(world.get_min_boxes(8), 25)
-        self.assertEqual(world.get_min_boxes(9), 31)
-        self.assertEqual(world.get_min_boxes(0), -1)
-        self.assertEqual(world.get_min_boxes(-1), -1)
+        stack = Solution()
+        self.assertEqual(stack.get_min_blocks(1), 1)
+        self.assertEqual(stack.get_min_blocks(2), 3)
+        self.assertEqual(stack.get_min_blocks(3), 5)
+        self.assertEqual(stack.get_min_blocks(4), 8)
+        self.assertEqual(stack.get_min_blocks(5), 11)
+        self.assertEqual(stack.get_min_blocks(6), 15)
+        self.assertEqual(stack.get_min_blocks(7), 20)
+        self.assertEqual(stack.get_min_blocks(8), 25)
+        self.assertEqual(stack.get_min_blocks(9), 31)
+        self.assertEqual(stack.get_min_blocks(0), 0)
+        self.assertEqual(stack.get_min_blocks(-1), 0)
 
 
 if __name__ == "__main__":
